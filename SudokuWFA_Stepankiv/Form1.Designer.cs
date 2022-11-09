3cb895c72b433a631b4e2831f905bfbd60ad9289
Sofia <71558524+sofia1337@users.noreply.github.com>
2022-11-09 13:40:11 +0200

    Add project files.

---
+++ SudokuWFA_Stepankiv/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace SudokuWFA_Stepankiv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.beginnerLevel = new System.Windows.Forms.RadioButton();
+            this.IntermediateLevel = new System.Windows.Forms.RadioButton();
+            this.AdvancedLevel = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(77, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(530, 505);
+            this.panel1.TabIndex = 0;
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(688, 41);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(156, 48);
+            this.checkButton.TabIndex = 1;
+            this.checkButton.Text = "checkButton";
+            this.checkButton.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(694, 119);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(149, 40);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "clearButton";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Location = new System.Drawing.Point(696, 184);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(146, 49);
+            this.newGameButton.TabIndex = 3;
+            this.newGameButton.Text = "newGameButton";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            // 
+            // beginnerLevel
+            // 
+            this.beginnerLevel.AutoSize = true;
+            this.beginnerLevel.Location = new System.Drawing.Point(723, 355);
+            this.beginnerLevel.Name = "beginnerLevel";
+            this.beginnerLevel.Size = new System.Drawing.Size(119, 21);
+            this.beginnerLevel.TabIndex = 4;
+            this.beginnerLevel.TabStop = true;
+            this.beginnerLevel.Text = "beginnerLevel";
+            this.beginnerLevel.UseVisualStyleBackColor = true;
+            // 
+            // IntermediateLevel
+            // 
+            this.IntermediateLevel.AutoSize = true;
+            this.IntermediateLevel.Location = new System.Drawing.Point(716, 393);
+            this.IntermediateLevel.Name = "IntermediateLevel";
+            this.IntermediateLevel.Size = new System.Drawing.Size(141, 21);
+            this.IntermediateLevel.TabIndex = 5;
+            this.IntermediateLevel.TabStop = true;
+            this.IntermediateLevel.Text = "IntermediateLevel";
+            this.IntermediateLevel.UseVisualStyleBackColor = true;
+            // 
+            // AdvancedLevel
+            // 
+            this.AdvancedLevel.AutoSize = true;
+            this.AdvancedLevel.Location = new System.Drawing.Point(714, 427);
+            this.AdvancedLevel.Name = "AdvancedLevel";
+            this.AdvancedLevel.Size = new System.Drawing.Size(126, 21);
+            this.AdvancedLevel.TabIndex = 6;
+            this.AdvancedLevel.TabStop = true;
+            this.AdvancedLevel.Text = "AdvancedLevel";
+            this.AdvancedLevel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 561);
+            this.Controls.Add(this.AdvancedLevel);
+            this.Controls.Add(this.IntermediateLevel);
+            this.Controls.Add(this.beginnerLevel);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.RadioButton beginnerLevel;
+        private System.Windows.Forms.RadioButton IntermediateLevel;
+        private System.Windows.Forms.RadioButton AdvancedLevel;
+    }
+}
+
